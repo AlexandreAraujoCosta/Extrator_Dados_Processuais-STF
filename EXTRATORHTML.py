51887adc3da5287427ca6a0b475f68559a92cd77
Alexandre Araújo Costa <alexandre.araujo.costa@gmail.com>
2024-08-09 09:37:36 -0300

    Update EXTRATORHTML.py

--- EXTRATORHTML.py
+++ EXTRATORHTML.py
@@ -1,6 +1,7 @@
 ## Extrair as informacoes dos arquivos
 
 import requests
+import dsl
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,198 +10,97 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
 
-driver = webdriver.Chrome()
+
+
+from selenium.webdriver.chrome.options import Options
+ 
+# Define a custom user agent
+my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+ 
+# Set the custom User-Agent
+chrome_options.add_argument(f"--user-agent={my_user_agent}")
+
+# Create a new instance of ChromeDriver with the desired options
+driver = webdriver.Chrome(options=chrome_options)
+
 # Set an implicit wait
 driver.implicitly_wait(20)
 
 
-   
 
 def waitForLoad(inputXPath): 
 
     Wait = WebDriverWait(driver, 20)       
     Wait.until(EC.presence_of_element_located((By.XPATH, inputXPath)))
     
+def xpath_get (xpath):
+    
+    waitForLoad(xpath)
+    dados = driver.find_element(By.XPATH, xpath).get_attribute('innerHTML')
+    
+    return dados
 
-inicio = 4352
-fim = 6300
+lista = []
+
+processo = 1000
 classe = "ADI"
 
-for n in range (fim-inicio):
+url = 'https://portal.stf.jus.br/processos/listarProcessos.asp?classe=' + classe + '&numeroProcesso=' + str(processo)
 
-    
-    num = str(n+inicio)
+dados = driver.get(url)
 
-    url = 'http://www.stf.jus.br/portal/processo/verProcessoAndamento.asp?numero='+ num + '&classe= ' + classe
+resumo = xpath_get('//*[@id="texto-pagina-interna"]')
 
-    html = requests.get(url).text
-    
+andamentos = xpath_get('//*[@id="texto-pagina-interna"]')
+
+dados_processuais = xpath_get('//*[@id="informacoes"]')
+
+partes = xpath_get('//*[@id="partes"]')
+
+decisoes = xpath_get('//*[@id="decisoes"]')
+
+sessaovirtual = xpath_get('//*[@id="sessao-virtual"]')
+
+deslocamentos = xpath_get('//*[@id="deslocamentos"]')
+
+peticoes = xpath_get('//*[@id="peticoes"]')
+
+recursos = xpath_get('//*[@id="recursos"]')
+
+pautas = xpath_get('//*[@id="pautas"]')
 
 
-    if  html.find("Este processo não consta em nossa base de Acompanhamento Processual") > 0:
-    
-        resumo = "Este processo não consta em nossa base de Acompanhamento Processual"
-
-        arquivo = "dados\\" + classe + num + "html.txt"
-
-        file = open(arquivo, "w+", encoding="utf-8")
-        file.write(str(url) + ", urlfim, " + resumo)
-        file.close()    
-
-    else:
-
-        driver.get(url)
-
-        time.sleep(3)
-    
-        dados = driver.find_element_by_xpath('//*[@id="texto-pagina-interna"]')
-        waitForLoad('//*[@id="texto-pagina-interna"]')
-        resumo= dados.get_attribute('innerHTML')
-
-        dados2 = driver.find_element_by_xpath('//*[@id="andamentos"]')
-        waitForLoad('//*[@id="andamentos"]')
-        andamentos= dados2.get_attribute('innerHTML')
+dados_a_gravar = [classe + str(processo), 
+          resumo,
+          andamentos,
+          dados_processuais,
+          partes,
+          decisoes, 
+          sessaovirtual, 
+          deslocamentos, 
+          peticoes, 
+          recursos,
+          pautas]
         
-        dados3 = driver.find_element_by_xpath('//*[@id="informacoes"]')
-        waitForLoad('//*[@id="informacoes"]')
-        dadosprocessuais= dados3.get_attribute('innerHTML')
+colunas = ['processo', 
+          'resumo',
+          'andamentos',
+          'dados_processuais',
+          'partes',
+          'decisoes', 
+          'sessaovirtual', 
+          'deslocamentos', 
+          'peticoes', 
+          'recursos',
+          'pautas']
 
-        dados4 = driver.find_element_by_xpath('//*[@id="partes"]')
-        waitForLoad('//*[@id="partes"]')
-        partes= dados4.get_attribute('innerHTML')
+lista.append(dados_a_gravar)
 
-        dados5 = driver.find_element_by_xpath('//*[@id="decisoes"]')
-        waitForLoad('//*[@id="decisoes"]')
-        decisoes= dados5.get_attribute('innerHTML')
-
-        dados6 = driver.find_element_by_xpath('//*[@id="sessao-virtual"]')
-        waitForLoad('//*[@id="sessao-virtual"]')
-        sessaovirtual= dados6.get_attribute('innerHTML')
-        
-        dados7 = driver.find_element_by_xpath('//*[@id="deslocamentos"]')
-        waitForLoad('//*[@id="deslocamentos"]')
-        deslocamentos= dados7.get_attribute('innerHTML')
-        
-        dados8 = driver.find_element_by_xpath('//*[@id="peticoes"]')
-        waitForLoad('//*[@id="peticoes"]')
-        peticoes= dados8.get_attribute('innerHTML')
-        
-        dados9 = driver.find_element_by_xpath('//*[@id="recursos"]')
-        waitForLoad('//*[@id="recursos"]')
-        recursos= dados9.get_attribute('innerHTML')
-
-        dados10 = driver.find_element_by_xpath('//*[@id="pautas"]')
-        waitForLoad('//*[@id="pautas"]')
-        pautas= dados10.get_attribute('innerHTML')
-
-        dadostotais = (str(url) + ", urlfim, " + resumo + ">>>>>" + dadosprocessuais + ">>>>>" + partes + ">>>>>" + andamentos + ">>>>>" + decisoes + ">>>>>" + sessaovirtual + ">>>>>" + deslocamentos + ">>>>>" + peticoes + ">>>>>" + recursos + ">>>>>" + pautas)
-
-        if dadostotais.find(">>>>>>>>") > 0:
-
-            driver.get(url)
-
-            time.sleep(3)
-        
-            dados = driver.find_element_by_xpath('//*[@id="texto-pagina-interna"]')
-            waitForLoad('//*[@id="texto-pagina-interna"]')
-            resumo= dados.get_attribute('innerHTML')
-
-            dados2 = driver.find_element_by_xpath('//*[@id="andamentos"]')
-            waitForLoad('//*[@id="andamentos"]')
-            andamentos= dados2.get_attribute('innerHTML')
-            
-            dados3 = driver.find_element_by_xpath('//*[@id="informacoes"]')
-            waitForLoad('//*[@id="informacoes"]')
-            dadosprocessuais= dados3.get_attribute('innerHTML')
-
-            dados4 = driver.find_element_by_xpath('//*[@id="partes"]')
-            waitForLoad('//*[@id="partes"]')
-            partes= dados4.get_attribute('innerHTML')
-
-            dados5 = driver.find_element_by_xpath('//*[@id="decisoes"]')
-            waitForLoad('//*[@id="decisoes"]')
-            decisoes= dados5.get_attribute('innerHTML')
-
-            dados6 = driver.find_element_by_xpath('//*[@id="sessao-virtual"]')
-            waitForLoad('//*[@id="sessao-virtual"]')
-            sessaovirtual= dados6.get_attribute('innerHTML')
-            
-            dados7 = driver.find_element_by_xpath('//*[@id="deslocamentos"]')
-            waitForLoad('//*[@id="deslocamentos"]')
-            deslocamentos= dados7.get_attribute('innerHTML')
-            
-            dados8 = driver.find_element_by_xpath('//*[@id="peticoes"]')
-            waitForLoad('//*[@id="peticoes"]')
-            peticoes= dados8.get_attribute('innerHTML')
-            
-            dados9 = driver.find_element_by_xpath('//*[@id="recursos"]')
-            waitForLoad('//*[@id="recursos"]')
-            recursos= dados9.get_attribute('innerHTML')
-
-            dados10 = driver.find_element_by_xpath('//*[@id="pautas"]')
-            waitForLoad('//*[@id="pautas"]')
-            pautas= dados10.get_attribute('innerHTML')
-
-            dadostotais = (str(url) + ", urlfim, " + resumo + ">>>>>" + dadosprocessuais + ">>>>>" + partes + ">>>>>" + andamentos + ">>>>>" + decisoes + ">>>>>" + sessaovirtual + ">>>>>" + deslocamentos + ">>>>>" + peticoes + ">>>>>" + recursos + ">>>>>" + pautas)
-
-            if dadostotais.find(">>>>>>>>") > 0:
-
-                driver.get(url)
-
-                time.sleep(3)
-            
-                dados = driver.find_element_by_xpath('//*[@id="texto-pagina-interna"]')
-                waitForLoad('//*[@id="texto-pagina-interna"]')
-                resumo= dados.get_attribute('innerHTML')
-
-                dados2 = driver.find_element_by_xpath('//*[@id="andamentos"]')
-                waitForLoad('//*[@id="andamentos"]')
-                andamentos= dados2.get_attribute('innerHTML')
-                
-                dados3 = driver.find_element_by_xpath('//*[@id="informacoes"]')
-                waitForLoad('//*[@id="informacoes"]')
-                dadosprocessuais= dados3.get_attribute('innerHTML')
-
-                dados4 = driver.find_element_by_xpath('//*[@id="partes"]')
-                waitForLoad('//*[@id="partes"]')
-                partes= dados4.get_attribute('innerHTML')
-
-                dados5 = driver.find_element_by_xpath('//*[@id="decisoes"]')
-                waitForLoad('//*[@id="decisoes"]')
-                decisoes= dados5.get_attribute('innerHTML')
-
-                dados6 = driver.find_element_by_xpath('//*[@id="sessao-virtual"]')
-                waitForLoad('//*[@id="sessao-virtual"]')
-                sessaovirtual= dados6.get_attribute('innerHTML')
-                
-                dados7 = driver.find_element_by_xpath('//*[@id="deslocamentos"]')
-                waitForLoad('//*[@id="deslocamentos"]')
-                deslocamentos= dados7.get_attribute('innerHTML')
-                
-                dados8 = driver.find_element_by_xpath('//*[@id="peticoes"]')
-                waitForLoad('//*[@id="peticoes"]')
-                peticoes= dados8.get_attribute('innerHTML')
-                
-                dados9 = driver.find_element_by_xpath('//*[@id="recursos"]')
-                waitForLoad('//*[@id="recursos"]')
-                recursos= dados9.get_attribute('innerHTML')
-
-                dados10 = driver.find_element_by_xpath('//*[@id="pautas"]')
-                waitForLoad('//*[@id="pautas"]')
-                pautas= dados10.get_attribute('innerHTML')
-
-                dadostotais = (str(url) + ", urlfim, " + resumo + ">>>>>" + dadosprocessuais + ">>>>>" + partes + ">>>>>" + andamentos + ">>>>>" + decisoes + ">>>>>" + sessaovirtual + ">>>>>" + deslocamentos + ">>>>>" + peticoes + ">>>>>" + recursos + ">>>>>" + pautas)
-
-
-        arquivo = "dados\\" + classe + num + "html.txt"
-
-        file = open(arquivo, "w+", encoding="utf-8")
-        file.write(dadostotais)
-        file.close()   
-
-
-            
-
-        
-    
+df = pd.DataFrame(lista, columns = colunas)
+df.to_csv('Dados_processuais.csv', index=False)
